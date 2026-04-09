b2c14e3a0a39a1ec252bfdb5086dd715766d10ca
sdsingh <samanta.singhD@gmail.com>
2026-04-09 16:25:49 -0700

    modified nav bar

--- petal-path/src/Components/Navbar/navbar.jsx
+++ petal-path/src/Components/Navbar/navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <img src="" alt="" className="logo"/>
-
+<h1>Welcome to Petal Path</h1>
             <ul>
                 <li>Petal Path</li>
                 <li>Discover</li>
