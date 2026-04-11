6ca7f26f00140bde9332331db500198984099565
sdsingh <samanta.singhD@gmail.com>
2026-04-11 11:39:19 -0700

    modified navbar and styling

--- petal-path/src/Components/Navbar/navbar.jsx
+++ petal-path/src/Components/Navbar/navbar.jsx
@@ -1,17 +1,20 @@
 import "./navbar.css"
+import logo from "../../assets/logo.png"
 
 const Navbar = () => {
     return (
         <div className="navbar">
-            <img src="" alt="" className="logo"/>
+            <img src= {logo} alt = "Petal Path Logo"className="logo"/>
 
             <ul>
-                <li>Petal Path</li>
+                <li>About</li>
                 <li>Discover</li>
                 <li>Events</li>
-                <li>Sign In</li>
             </ul>
-            
+
+            <div className="signin">Sign In</div>
+
+
         </div>
     )
 }
