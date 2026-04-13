d2401de351acf11b0b396c3c0e59eb92dfe5f93f
sdsingh <samanta.singhD@gmail.com>
2026-04-13 08:30:04 -0700

    reworked the header
    refactored header snd fixed stylind conflicts

--- petal-path/src/Components/Navbar/navbar.jsx
+++ petal-path/src/Components/Navbar/navbar.jsx
@@ -2,29 +2,22 @@ import "./Navbar.css";
 import logo from "../../assets/logo.png";
 
 const Navbar = () => {
-    return (
-<<<<<<< HEAD
-        <div className="navbar">
-            <div className="logo-container">
-                <img src={logo} alt="Petal Path Logo" className="logo" />
-                <div className="logo-text">Petal Path</div>
-            </div>
+  return (
+    <div className="navbar">
+      <div className="logo-container">
+        <img src={logo} alt="Petal Path Logo" className="logo" />
+        <div className="logo-text">Petal Path</div>
+      </div>
 
-=======
- <div className="navbar">
-            <div className = "logo-container"> 
-            <img src= {logo} alt = "Petal Path Logo"className="logo"/>
-            <div className = "logo-text">Petal Path</div>
-        </div>
-        
->>>>>>> a1c4016 (reworked the header)
-            <ul>
-                <li>About</li>
-                <li>Discover</li>
-                <li>Events</li>
-            </ul>
+      <ul>
+        <li>About</li>
+        <li>Discover</li>
+        <li>Events</li>
+      </ul>
 
-            <div className="signin">Sign In</div>
-        </div>
-    );
+      <div className="signin">Sign In</div>
+    </div>
+  );
 };
+
+export default Navbar;
