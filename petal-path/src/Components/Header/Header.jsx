9f329e67de2ffd7aa0969e77087777ec84b1fdac
sdsingh <samanta.singhD@gmail.com>
2026-04-13 10:05:08 -0700

    fixed merge conflict in Header

--- petal-path/src/Components/Header/Header.jsx
+++ petal-path/src/Components/Header/Header.jsx
@@ -1,21 +1,12 @@
 import "./Header.css";
-<<<<<<< HEAD
-import Navbar from "../Navbar/Navbar";
-=======
 import Navbar from "../Navbar/navbar";
->>>>>>> a1c4016 (reworked the header)
-
-//adding the navbar feature into my header component
 
 const Header = () => {
     return (
-        <header className = "header">
-            <Navbar/>
+        <header className="header">
+            <Navbar />
         </header>
-    )
-}
-<<<<<<< HEAD
+    );
+};
 
-=======
->>>>>>> a1c4016 (reworked the header)
 export default Header;
